01ad2db931a3ad95ae46f5a75182a68a974a3da6
Tibosity <89561185+Tibosity@users.noreply.github.com>
2022-06-02 18:50:25 -0400

    Update fakespeedglch.lua

--- fakespeedglch.lua
+++ fakespeedglch.lua
@@ -17,12 +17,12 @@ function onButton1Up(mouse)
 end
  
 function onSelected(mouse)
-    mouse.KeyDown:connect(function(q) if q:lower()=="q"then onButton1Down(mouse)end end)
-    mouse.KeyUp:connect(function(q) if q:lower()=="q"then onButton1Up(mouse)end end)
+    mouse.KeyDown:connect(function(z) if q:lower()=="z"then onButton1Down(mouse)end end)
+    mouse.KeyUp:connect(function(z) if q:lower()=="z"then onButton1Up(mouse)end end)
 end
 onSelected(game.Players.LocalPlayer:GetMouse())
 game.StarterGui:SetCore("SendNotification",{
-			Title = "Keybind is Q";
+			Title = "Keybind is Z";
 			Text = "AntiSec";
 			Duration = 0.20;
 		})
